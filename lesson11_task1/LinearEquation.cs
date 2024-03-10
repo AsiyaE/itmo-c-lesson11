4319b81f78cc309ec21ff6b2570ecb144b87aeb3
Asiya Ermolaeva <asiya_ermolaeva@mail.ru>
2024-03-10 15:24:54 +0300

    Добавьте файлы проекта.

---
+++ lesson11_task1/LinearEquation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson11_task1
+{
+    struct LinearEquation
+    {
+        double k;
+        double b;
+
+        public LinearEquation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            if (k == 0){
+                if (b == 0) return " x - любое число";
+                return "нет решений";
+            }
+            else
+            {
+                return $"x = {((0 - b) / k)}";
+            }
+        }
+    }
+}
---
+++ lesson11_task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson11_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа k и b для уравнения 0=kx+b");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+
+            LinearEquation eq = new LinearEquation(k, b);
+            Console.WriteLine(eq.Root());
+            Console.ReadKey();
+        }
+    }
+}
